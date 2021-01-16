5e74a2b54a68488f5ca3204b37cc1e97c9468327
jallend1 <jallend1@gmail.com>
2021-01-15 16:12:22 -0800

    Moves Victory conditions out of global state into checkVictory. Adds code to prevent game from continuing after game over

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -2,40 +2,41 @@ import { useState, useEffect } from 'react';
 import Square from  './Square';
 
 const Board = () => {
-    const victories = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 4, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [2, 4, 6]
-    ];
     
-    const [gameStatus, setGameStatus] = useState(Array(9).fill(null));
-    const [isX, setIsX] = useState(true);
-    const [isGameOver, setIsGameOver] = useState(false);
-
-    const checkVictory = () => {
+    const [gameStatus, setGameStatus] = useState(Array(9).fill(null));  // Gameboard
+    const [isX, setIsX] = useState(true);                               // X or O's turn
+    const [isGameOver, setIsGameOver] = useState(false);                // Game over?
+    
+    const checkVictory = () => {                                        //Compares current board to possible victories
+        const victories = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 4, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [2, 4, 6]
+        ];
         victories.forEach(([a, b, c]) => {
             if( gameStatus[a] && 
                 gameStatus[b] === gameStatus[a] && 
                 gameStatus[c] === gameStatus[a]){
                     setIsGameOver(true);
                 }
-        })
-
+        });
     }
 
     const handleClick = cellLocation => {
+        if(isGameOver) return;                                      // If game is over, prevents further mucking about
+
         const currentStatus = gameStatus.slice();
         isX ? currentStatus[cellLocation] = 'X' : currentStatus[cellLocation] = 'O';
         setIsX(!isX);
         setGameStatus(currentStatus);
     }
 
-    useEffect(checkVictory, [gameStatus])
+    useEffect(checkVictory, [gameStatus]);                          // Checks for victory conditions after current board is updated
 
     return (
     <>
