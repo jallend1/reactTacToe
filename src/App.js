8923a5b64b588dc9ce678920a5e88b772fbcf038
jallend1 <jallend1@gmail.com>
2021-01-16 14:27:27 -0800

    Lifts gameFinished state up to App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import { useState } from 'react';
+
 import './index.css';
 import Board from './Components/Board'
 
 const App = () => {
+    const [isGameOver, setIsGameOver] = useState(false);                // Game over?
+    const checkVictory = (gameStatus) => {                                        //Compares current board to possible victories
+        const victories = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 4, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [2, 4, 6]
+        ];
+        victories.forEach(([a, b, c]) => {
+            if( gameStatus[a] && 
+                gameStatus[b] === gameStatus[a] && 
+                gameStatus[c] === gameStatus[a]){
+                    setIsGameOver(true);
+                }
+        });
+    }
     return (
         <>
             <h1>React Tac Toe</h1>
-            <Board />
+            <h2>Is the game over? {isGameOver ? <h2>Yes!</h2> : <h2>No!</h2>}</h2>
+            <Board isGameOver={isGameOver} checkVictory={checkVictory}/>
         </>
     )
 }
--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,32 +1,11 @@
 import { useState, useEffect } from 'react';
 import Square from  './Square';
 
-const Board = () => {
+const Board = ({isGameOver, checkVictory}) => {
     
     const [gameStatus, setGameStatus] = useState(Array(9).fill(null));  // Gameboard
     const [isX, setIsX] = useState(true);                               // X or O's turn
-    const [isGameOver, setIsGameOver] = useState(false);                // Game over?
-    
-    const checkVictory = () => {                                        //Compares current board to possible victories
-        const victories = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 4, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [2, 4, 6]
-        ];
-        victories.forEach(([a, b, c]) => {
-            if( gameStatus[a] && 
-                gameStatus[b] === gameStatus[a] && 
-                gameStatus[c] === gameStatus[a]){
-                    setIsGameOver(true);
-                }
-        });
-    }
-
+      
     const handleClick = cellLocation => {
         if(isGameOver) return;                                      // If game is over, prevents further mucking about
         const currentStatus = gameStatus.slice();
@@ -35,13 +14,10 @@ const Board = () => {
         setGameStatus(currentStatus);
     }
 
-    useEffect(checkVictory, [gameStatus]);                          // Checks for victory conditions after current board is updated
+    useEffect(() => checkVictory(gameStatus), [gameStatus, checkVictory]);                          // Checks for victory conditions after current board is updated
 
     return (
     <>
-        <header>
-            <p>Is the game over? {isGameOver ? 'Yes!' : 'No'}</p>
-        </header>
         <div className="row">
             <Square cellLocation={0} val={gameStatus[0]} onClick={handleClick}/>
             <Square cellLocation={1} val={gameStatus[1]} onClick={handleClick}/>
