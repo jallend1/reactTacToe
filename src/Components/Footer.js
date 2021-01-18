4a9a98abb639e6853b67e17c9d4ac224dc2af5aa
jallend1 <jallend1@gmail.com>
2021-01-18 13:48:30 -0800

    Cleans up victory logic and returns a play again button

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,13 @@
 const Footer = ({isGameOver, isX, gameStatus}) => {
     const victoryState = () => {
         if(isGameOver){
-            if(isX){
-                return <h2>O is the winner!</h2>
-            }
-            if(!isX){
-                return <h2>X is the winner!</h2>
-            }
+            const winner = isX ? 'O' : 'X';
+            return (
+                <div className="victory">
+                    <h2>{winner} is the winner!</h2>
+                    <button className="mdl-button mdl-js-button mdl-button--raised mdl-button--accent history-button">Play Again?</button>
+                </div>
+            )
         }
         else if(gameStatus.indexOf(null) === -1){
             return <h2>We've got a draw, folks!</h2>
