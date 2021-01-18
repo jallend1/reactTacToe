a65e0477111a1ce1c570f2e4a94bbd27258feba2
jallend1 <jallend1@gmail.com>
2021-01-18 13:15:05 -0800

    Implements MDL-Lite styling library

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,12 @@
 const Header = ({isGameOver, isX}) => {
     return (
-        <header>
-            <h1>React Tac Toe</h1>
-        </header>
+        <div className="mdl-layout mdl-layout--fixed-header">    
+            <header className="mdl-layout__header">
+                <div className="mdl-layout__header-row">
+                    <h1 className="mdl-layout-title">React Tac Toe</h1>
+                </div>
+            </header>
+        </div>
     )
 }
 
--- src/Components/History.js
+++ src/Components/History.js
@@ -2,13 +2,13 @@ const History = ({history, goBack}) => {
     const buttons = history.map((gameboard, index) => {
         if(history.length > 1){                                                                         // Only continues if there's more than just the initial blank board
             if(index === 0){
-                return <button onClick={() => goBack(index) } key={index}>Go back to start</button>;
+                return <button onClick={() => goBack(index) } key={index} className="mdl-button mdl-js-button mdl-button--raised mdl-button--accent">Go back to start</button>;
             }
             else if(index === history.length - 1){
                 return null;
             }
             else{
-                return <button onClick={() => goBack(index) } key={index}>Go back to move {index}</button>
+                return <button onClick={() => goBack(index) } key={index} className="mdl-button mdl-js-button mdl-button--raised mdl-button--accent history-button">Go back to move {index}</button>
             }
                 
         }
